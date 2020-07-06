480f7c79f01b6693a1b3f0a89ab6e72313785108
yoonminsang <apdlyooapdl@naver.com>
2020-07-06 19:05:01 +0900

    final

---
+++ src/containers/AddNumber.js
@@ -0,0 +1,26 @@
+import AddNumber from "../components/AddNumber";
+import { connect } from "react-redux";
+function mapReduxDispatchToReactProps(dispatch) {
+  return {
+    onClick: function (size) {
+      dispatch({ type: "INCREMENT", size: size });
+    },
+  };
+}
+export default connect(null, mapReduxDispatchToReactProps)(AddNumber);
+
+/*
+import React, { Component } from "react";
+import store from "../store";
+export default class extends Component {
+  render() {
+    return (
+      <AddNumber
+        onClick={function (size) {
+          store.dispatch({ type: "INCREMENT", size: size });
+        }}
+      ></AddNumber>
+    );
+  }
+}
+*/
